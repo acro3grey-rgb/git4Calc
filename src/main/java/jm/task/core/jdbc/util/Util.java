9861ce0bd2821f39bdb140c33ac2870120a06da5
Sergei Pelevin <greyacromachine@gmail.com>
2026-02-20 14:16:32 +0500

    Refactor Hibernate SessionFactory initialization

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -13,7 +13,6 @@ public class Util {
     // ===== JDBC =====
     private static final String URL =
             "jdbc:mysql://localhost:3306/usersbd?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-
     private static final String USER = "root";
     private static final String PASSWORD = "Grey3run!";
 
@@ -26,29 +25,35 @@ public class Util {
     }
 
     // ===== Hibernate =====
-    private static SessionFactory sessionFactory;
+    private static final SessionFactory SESSION_FACTORY = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            Configuration configuration = new Configuration();
+
+            configuration.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
+            configuration.setProperty("hibernate.connection.url", URL);
+            configuration.setProperty("hibernate.connection.username", USER);
+            configuration.setProperty("hibernate.connection.password", PASSWORD);
+
+            configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+            configuration.setProperty("hibernate.show_sql", "false");
+            configuration.setProperty("hibernate.hbm2ddl.auto", "none"); // таблицы создаём/удаляем вручную SQL по ТЗ
+
+            configuration.addAnnotatedClass(User.class);
+
+            return configuration.buildSessionFactory();
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to build SessionFactory", e);
+        }
+    }
 
     public static SessionFactory getSessionFactory() {
-        if (sessionFactory == null) {
-            try {
-                Configuration configuration = new Configuration();
+        return SESSION_FACTORY;
+    }
 
-                configuration.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
-                configuration.setProperty("hibernate.connection.url", URL);
-                configuration.setProperty("hibernate.connection.username", USER);
-                configuration.setProperty("hibernate.connection.password", PASSWORD);
-
-                configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
-                configuration.setProperty("hibernate.show_sql", "false");
-                configuration.setProperty("hibernate.hbm2ddl.auto", "none"); // таблицу создаём сами через SQL
-
-                configuration.addAnnotatedClass(User.class);
-
-                sessionFactory = configuration.buildSessionFactory();
-            } catch (Exception e) {
-                throw new RuntimeException("Failed to build SessionFactory", e);
-            }
-        }
-        return sessionFactory;
+    // Не обязательно для тестов, но полезно (например, если будешь закрывать приложение вручную)
+    public static void shutdown() {
+        SESSION_FACTORY.close();
     }
 }
